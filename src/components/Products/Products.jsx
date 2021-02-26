6e42fb4959bf253bdc093dd0516fcaa300c9e435
Erick Camacho <ercamdev@gmail.com>
2021-02-26 16:33:01 -0600

    Setup & create Product components

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,23 @@
+import { Grid } from '@material-ui/core';
+import Product from './Product/Product'
+
+const products = [
+  {id: 1, name: 'Shoes', description: 'Running shoes', price: '$5', image:'https://images.pexels.com/photos/1032110/pexels-photo-1032110.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+  {id: 2, name: 'Macbook', description: 'Apple macbook', price:'$10', image:'https://images.pexels.com/photos/249538/pexels-photo-249538.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'},
+]
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+        {products.map(product => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product}/>
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+}
+ 
+export default Products;
